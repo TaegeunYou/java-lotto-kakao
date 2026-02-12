90a87316cd99fd43d28b0320a91d31948700047d
YouTaegeun <dbxorms2310@naver.com>
2026-02-12 14:36:01 +0900

    fix: 중복 상수 제거

--- src/main/java/domain/lotto/LottoNumber.java
+++ src/main/java/domain/lotto/LottoNumber.java
@@ -5,11 +5,9 @@ import java.util.Objects;
 public class LottoNumber {
 
     private final int number;
-    public static final int MIN_NUMBER = 1;
-    public static final int MAX_NUMBER = 45;
 
     public LottoNumber(int number) {
-        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+        if (number < Lotto.MIN_NUMBER || number > Lotto.MAX_NUMBER) {
             throw new IllegalArgumentException("로또 번호는 1부터 45 사이여야 합니다.");
         }
         this.number = number;
